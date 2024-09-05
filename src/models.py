c7328296af4bd1b3a670f0d28dda71bc5ac2b4fd
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-09-05 20:12:09 +0300

    Создал родительский класс меню, от которого наследую MainMenu, CategoryMenu и LevelMenu, чтобы не повторять код

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import random
 from abc import ABC, abstractmethod
+from lib2to3.pgen2.grammar import opmap
 
 from utils import clear_screen
 
@@ -52,17 +53,35 @@ class Session:
 
 
 class Menu:
+    options: list
+    exit = False
+    selected_index: int = 0
+
+    def exit_menu(self):
+        self.exit = True
+
+    def print_menu(self):
+        clear_screen()
+        print("Выберите опцию:")
+        for i, option in enumerate(self.options):
+            if i == self.selected_index:
+                print(f"> {option}")
+            else:
+                print(f"  {option}")
+
+
+class MainMenu(Menu):
     options: list = ['Начать игру',
                      'Выбрать категорию',
                      'Выбрать сложность',
                      'Добавить слово',
                      'Удалить слово',
                      'Выйти']
-    selected_index: int = 0
+    # selected_index: int = 0
     LIST_OF_WORDS = []
     LIST_OF_CATEGORIES = []
     LIST_OF_LEVELS = []
-    exit = False
+    # exit = False
 
     def __init__(self):
         self.users_category: str
@@ -83,17 +102,17 @@ class Menu:
     def delete_word(self):
         pass
 
-    def exit_menu(self):
-        self.exit = True
-
-    def print_menu(self):
-        clear_screen()
-        print("Выберите опцию:")
-        for i, option in enumerate(self.options):
-            if i == self.selected_index:
-                print(f"> {option}")
-            else:
-                print(f"  {option}")
+    # def exit_menu(self):
+    #     self.exit = True
+    #
+    # def print_menu(self):
+    #     clear_screen()
+    #     print("Выберите опцию:")
+    #     for i, option in enumerate(self.options):
+    #         if i == self.selected_index:
+    #             print(f"> {option}")
+    #         else:
+    #             print(f"  {option}")
 
     def create_and_add_word(self, value, level, category, hint) -> None:
         """
@@ -106,12 +125,16 @@ class Menu:
         """
         word = Word(value, level, category, hint)
         self.LIST_OF_WORDS.append(word)
+        self.add_level(level)
+        self.add_category(category)
 
     def add_category(self, new_category) -> None:
-        self.LIST_OF_CATEGORIES.append(new_category)
+        if new_category not in self.LIST_OF_CATEGORIES:
+            self.LIST_OF_CATEGORIES.append(new_category)
 
     def add_level(self, new_level) -> None:
-        self.LIST_OF_LEVELS.append(new_level)
+        if new_level not in self.LIST_OF_LEVELS:
+            self.LIST_OF_LEVELS.append(new_level)
 
     def fill_lists(self) -> None:
         """
@@ -124,19 +147,31 @@ class Menu:
 
         self.create_and_add_word('монитор', 'легкий', 'компьютер', 'отображает картинку')
         self.create_and_add_word('ноутбук', 'средний', 'компьютер', 'портативный пк')
-        self.create_and_add_word('кулер', 'тяжелый', 'компьтер', 'охлаждает воздух')
+        self.create_and_add_word('кулер', 'тяжелый', 'компьютер', 'охлаждает воздух')
 
         self.create_and_add_word('велосипед', 'легкий', 'транспорт', 'имеет два колеса')
         self.create_and_add_word('скейт', 'средний', 'транспорт', 'связан с культурой одежды')
         self.create_and_add_word('гидроцикл', 'тяжелый', 'транспорт', 'движется по воде')
 
-        self.add_category('легкий')
-        self.add_category('средний')
-        self.add_category('тяжелый')
-
-        self.add_level('животные')
-        self.add_level('компьютер')
-        self.add_level('транспорт')
+        # self.add_category('легкий')
+        # self.add_category('средний')
+        # self.add_category('тяжелый')
+        #
+        # self.add_level('животные')
+        # self.add_level('компьютер')
+        # self.add_level('транспорт')
 
         self.users_category = random.choice(self.LIST_OF_CATEGORIES)
         self.users_level = random.choice(self.LIST_OF_LEVELS)
+
+
+class CategoryMenu(Menu):
+    options: list
+    def get_list_from_main_menu(self, main_menu: MainMenu):
+        self.options = main_menu.LIST_OF_CATEGORIES
+
+
+class LevelMenu(Menu):
+    options: list
+    def get_list_from_main_menu(self, main_menu: MainMenu):
+        self.options = main_menu.LIST_OF_LEVELS
