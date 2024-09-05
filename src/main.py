26cbca2fc12ab56bd1dccc6a1f2ff1d0f18571e8
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-09-05 17:30:24 +0300

    Сделал рабочее меню со стрелкой, используя библиотеку keyboard

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import logging
 import platform
+from time import sleep
 
-from src.models import Menu
-from src.utils import get_key
+from models import Menu
+import keyboard
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
@@ -16,14 +17,14 @@ def main() -> None:
     menu.fill_lists()
     while True:
         menu.print_menu()
-        key = get_key()
-        if key:
-            if ord(key) == 13:  # Enter key
-                return menu.options[menu.selected_index]
-            elif ord(key) == 65 or ord(key) == 72:  # Up arrow
-                menu.selected_index = (menu.selected_index - 1) % len(menu.options)
-            elif ord(key) == 66 or ord(key) == 80:  # Down arrow
-                menu.selected_index = (menu.selected_index + 1) % len(menu.options)
+        key = keyboard.read_key()
+        if key == 'enter':  # Enter key
+            print(menu.options[menu.selected_index])
+        elif key == 'up':  # Up arrow
+            menu.selected_index = (menu.selected_index - 1) % len(menu.options)
+        elif key == 'down':  # Down arrow
+            menu.selected_index = (menu.selected_index + 1) % len(menu.options)
+        sleep(0.1)
 
 
 if __name__ == "__main__":
