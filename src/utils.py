f5c3354a5b4320b3b8cc342d14700e363061f9f5
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-09-05 17:32:50 +0300

    Удалил get_key(), исправил clear_screen()

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,5 @@
 import os
-import random
-import sys
-# import tty
-
-import select
 
 
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-
-def get_key(termios=None):
-    if os.name == 'nt':
-        import msvcrt
-        return msvcrt.getch()
-    else:
-        def is_data():
-            return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
-
-        old_settings = termios.tcgetattr(sys.stdin)
-        try:
-            # tty.setcbreak(sys.stdin.fileno())
-            if is_data():
-                return sys.stdin.read(1)
-            return None
-        finally:
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    os.system('cls')
