186a0b627df6f3a6b2e01d5cf5eac4697746458a
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-09-06 18:09:58 +0300

    добавил в Word методы для преобразования в json и обратно с list. Написал add_word в MainMenu, а также заменил заполнение слов методом fill_lists на заполнение из json, чтобы после завершения одной сессии изменения слов сохранялись для других. Также вынес рандомайзер для категории и сложности в отдельную функцию

--- src/models.py
+++ src/models.py
@@ -1,6 +1,10 @@
+import json
 import random
 from abc import ABC, abstractmethod
 from lib2to3.pgen2.grammar import opmap
+from time import sleep
+
+import keyboard
 
 from utils import clear_screen
 
@@ -16,17 +20,22 @@ class Word:
         self.__hint = hint
         self.__length = len(self.__value)
 
-    # def set_value(self, value):
-    #     self.value = value
-    #
-    # def set_level(self, level):
-    #     self.level = level
-    #
-    # def set_category(self, category):
-    #     self.category = category
-    #
-    # def set_hint(self, hint):
-    #     self.hint = hint
+    def to_dict(self):
+        return {
+            'value': self.__value,
+            'level': self.__level,
+            'category': self.__category,
+            'hint': self.__hint
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            data['value'],
+            data['level'],
+            data['category'],
+            data['hint']
+        )
 
     def get_value(self):
         return self.__value
@@ -88,15 +97,14 @@ class MainMenu(Menu):
                      'Добавить слово',
                      'Удалить слово',
                      'Выйти']
-    # selected_index: int = 0
     LIST_OF_WORDS = []
     LIST_OF_CATEGORIES = []
     LIST_OF_LEVELS = []
-    # exit = False
 
-    def __init__(self):
+    def __init__(self, filename: str = 'word_list.json'):
         self.users_category: str
         self.users_level: str
+        self.filename = filename
 
     def start_game(self):
         pass
@@ -107,23 +115,24 @@ class MainMenu(Menu):
     def choice_level(self, new_level):
         self.users_level = new_level
 
-    def add_word(self, value, category, level, hint):
-        self.create_and_add_word(value, category, level, hint)
+    def add_word(self):
+        clear_screen()
+        print("Введите: слово, уровень сложности, категорию для этого слова, подсказку."
+              " Разделяя ', '. Для того чтобы вернуться нажмите 'enter+esc'\n")
+        sleep(0.15)
+        while True:
+            args = input().split(', ')
+            if len(args) == 4:
+                self.create_and_add_word(args[0], args[1], args[2], args[3])
+                break
+            else:
+                print('Вы где-то допустили ошибку!\n')
+                if keyboard.is_pressed('esc'):
+                    break
+
 
     def delete_word(self):
         pass
-
-    # def exit_menu(self):
-    #     self.exit = True
-    #
-    # def print_menu(self):
-    #     clear_screen()
-    #     print("Выберите опцию:")
-    #     for i, option in enumerate(self.options):
-    #         if i == self.selected_index:
-    #             print(f"> {option}")
-    #         else:
-    #             print(f"  {option}")
 
     def create_and_add_word(self, value, level, category, hint) -> None:
         """
@@ -138,42 +147,33 @@ class MainMenu(Menu):
         self.LIST_OF_WORDS.append(word)
         self.add_level(level)
         self.add_category(category)
+        self.save_words()
+
+    def save_words(self):
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            json.dump([word.to_dict() for word in self.LIST_OF_WORDS], f, ensure_ascii=False, indent=4)
+
+    def load_words(self):
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as f:
+                self.LIST_OF_WORDS = [Word.from_dict(data) for data in json.load(f)]
+                for i in range(len(self.LIST_OF_WORDS)):
+                    self.add_level(self.LIST_OF_WORDS[i].get_level())
+                    self.add_category(self.LIST_OF_WORDS[i].get_category())
+        except FileNotFoundError:
+            return []
+
+    def random_fields(self):
+        self.users_category = random.choice(self.LIST_OF_CATEGORIES)
+        self.users_level = random.choice(self.LIST_OF_LEVELS)
 
     def add_category(self, new_category) -> None:
         if new_category not in self.LIST_OF_CATEGORIES:
-            self.LIST_OF_CATEGORIES.append(new_category)
+            self.LIST_OF_CATEGORIES.insert(0, new_category)
 
     def add_level(self, new_level) -> None:
         if new_level not in self.LIST_OF_LEVELS:
-            self.LIST_OF_LEVELS.append(new_level)
-
-    def fill_lists(self) -> None:
-        """
-        Заполняем списки слов, категорий и сложностей
-        :return:
-        """
-        self.create_and_add_word('лев', 'легкий', 'животные', 'король зверей')
-        self.create_and_add_word('корова', 'средний', 'животные', 'лицо популярной шоколадки')
-        self.create_and_add_word('капибара', 'тяжелый', 'животные', 'любит арбузы')
-
-        self.create_and_add_word('монитор', 'легкий', 'компьютер', 'отображает картинку')
-        self.create_and_add_word('ноутбук', 'средний', 'компьютер', 'портативный пк')
-        self.create_and_add_word('кулер', 'тяжелый', 'компьютер', 'охлаждает воздух')
-
-        self.create_and_add_word('велосипед', 'легкий', 'транспорт', 'имеет два колеса')
-        self.create_and_add_word('скейт', 'средний', 'транспорт', 'связан с культурой одежды')
-        self.create_and_add_word('гидроцикл', 'тяжелый', 'транспорт', 'движется по воде')
-
-        # self.add_category('легкий')
-        # self.add_category('средний')
-        # self.add_category('тяжелый')
-        #
-        # self.add_level('животные')
-        # self.add_level('компьютер')
-        # self.add_level('транспорт')
-
-        self.users_category = random.choice(self.LIST_OF_CATEGORIES)
-        self.users_level = random.choice(self.LIST_OF_LEVELS)
+            self.LIST_OF_LEVELS.insert(0, new_level)
 
 
 class CategoryMenu(Menu):
