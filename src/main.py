20393b6d903f90797f031c82903d19e974a5150f
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-09-06 16:30:49 +0300

    Изменено управление основным и категорийным меню для использования метода handle_key. Удалены дублирующиеся условные операторы для обработки нажатий клавиш.

--- src/main.py
+++ src/main.py
@@ -19,10 +19,10 @@ def main() -> None:
 
     menu = MainMenu()
     menu.fill_lists()
-    while True:
+    while not menu.exit:
         menu.print_menu()
         key = keyboard.read_key()
-        if key == 'enter':  # Enter key
+        if menu.handle_key(key) == 'enter':
             if menu.options[menu.selected_index] == 'Начать игру':
                 menu.start_game()
                 """
@@ -37,18 +37,12 @@ def main() -> None:
                     sleep(0.1)
                     category_menu.print_menu()
                     category_key = keyboard.read_key()
-                    if category_key == 'enter':
+                    if category_menu.handle_key(category_key) == 'enter':
                         """
                         выбираем категорию
                         """
                         menu.choice_category(category_menu.options[category_menu.selected_index])
                         category_menu.exit_menu()
-                    elif category_key == 'up':  # Up arrow
-                        category_menu.selected_index = (category_menu.selected_index - 1) % len(category_menu.options)
-                    elif category_key == 'down':  # Down arrow
-                        category_menu.selected_index = (category_menu.selected_index + 1) % len(category_menu.options)
-                    elif category_key == 'esc' or category_menu.exit is True:
-                        break
                     sleep(0.1)
             elif menu.options[menu.selected_index] == 'Выбрать сложность':
                 """
@@ -65,13 +59,7 @@ def main() -> None:
                 menu.delete_word()
             elif menu.options[menu.selected_index] == 'Выйти':
                 menu.exit_menu()
-                continue
-        elif key == 'up':  # Up arrow
-            menu.selected_index = (menu.selected_index - 1) % len(menu.options)
-        elif key == 'down':  # Down arrow
-            menu.selected_index = (menu.selected_index + 1) % len(menu.options)
-        elif key == 'esc' or menu.exit is True:
-            break
+                break
         sleep(0.1)
 
 
