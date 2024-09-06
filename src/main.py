4635fc7979598fe1746e8b80e0a6c92fbb77d90b
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-09-06 18:07:16 +0300

    Добавил реализацию выбора сложности и добавления слова

--- src/main.py
+++ src/main.py
@@ -2,12 +2,10 @@ import logging
 import platform
 from time import sleep
 
-from unicodedata import category
-
 from models import MainMenu
 import keyboard
 
-from models import CategoryMenu
+from models import CategoryMenu, LevelMenu
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
@@ -18,7 +16,8 @@ def main() -> None:
     logger.info(platform.python_version())
 
     menu = MainMenu()
-    menu.fill_lists()
+    menu.load_words()
+    menu.random_fields()
     while not menu.exit:
         menu.print_menu()
         key = keyboard.read_key()
@@ -28,13 +27,14 @@ def main() -> None:
                 """
                 Реализовать внутренности(можно создавать объект сессии, туда закинуть все данные и продумать отрисовку)
                 """
+
             elif menu.options[menu.selected_index] == 'Выбрать категорию':
                 category_menu = CategoryMenu()
                 category_menu.get_list_from_main_menu(menu)
                 if 'Выйти' not in category_menu.options:
                     category_menu.options.append('Выйти')
                 while not category_menu.exit:
-                    sleep(0.1)
+                    sleep(0.15)
                     category_menu.print_menu()
                     category_key = keyboard.read_key()
                     if category_menu.handle_key(category_key) == 'enter':
@@ -43,24 +43,39 @@ def main() -> None:
                         """
                         menu.choice_category(category_menu.options[category_menu.selected_index])
                         category_menu.exit_menu()
-                    sleep(0.1)
+                    sleep(0.15)
+
             elif menu.options[menu.selected_index] == 'Выбрать сложность':
-                """
-                добавить менюшку где можно выбрать одну из существующий сложностей
-                """
-                # menu.choice_level()
+                level_menu = LevelMenu()
+                level_menu.get_list_from_main_menu(menu)
+                if 'Выйти' not in level_menu.options:
+                    level_menu.options.append('Выйти')
+                while not level_menu.exit:
+                    sleep(0.15)
+                    level_menu.print_menu()
+                    category_key = keyboard.read_key()
+                    if level_menu.handle_key(category_key) == 'enter':
+                        """
+                        выбираем уровень
+                        """
+                        menu.choice_level(level_menu.options[level_menu.selected_index])
+                        level_menu.exit_menu()
+                    sleep(0.15)
+
             elif menu.options[menu.selected_index] == 'Добавить слово':
                 """
-                можно почистить экран, и чтобы через пробел/интер вводились слово, категория итд                
+                Добавляем слово               
                 """
-                # menu.add_word()
-            elif menu.options[menu.selected_index] == 'Удалить слово':
 
+                menu.add_word()
+
+            elif menu.options[menu.selected_index] == 'Удалить слово':
                 menu.delete_word()
+
             elif menu.options[menu.selected_index] == 'Выйти':
                 menu.exit_menu()
                 break
-        sleep(0.1)
+        sleep(0.15)
 
 
 if __name__ == "__main__":
