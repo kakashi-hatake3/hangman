a312631430fcf84736c70001fa9c8ae46874c32b
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-09-14 17:14:28 +0300

    вынес весь сторонний функционал в utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,19 @@
 import os
+import time
+import random
 
 
 def clear_screen():
     os.system('cls')
+
+
+def sleep(seconds: float):
+    time.sleep(seconds)
+
+
+def random_choice(lst: list):
+    return random.choice(lst)
+
+
+def random_int(start, end):
+    return random.randint(start, end)
